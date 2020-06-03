0cec87ce521c182ac201e83eaf19be730b9414ed
amandanic11 <65553398+amandanic11@users.noreply.github.com>
2020-06-03 09:12:27 -0400

    filepath update

--- pandagroceries.py
+++ pandagroceries.py
@@ -5,7 +5,7 @@ import pandas
 import os
 
 #csv_filepath = "data/products.csv"
-csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "products.csv")
+csv_filepath = os.path.join(os.path.dirname(__file__), "data", "products.csv")
 df = pandas.read_csv(csv_filepath)
 print(type(df)) #> <class 'pandas.core.frame.DataFrame'>
 print(df.head()) #> prints first few rows
